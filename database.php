6a8ea1d85f0b0f40a1155555bff2dd30500f5039
SebastianXXI05 <kaynandredblue@gmail.com>
2023-07-01 21:25:39 -0500

    update database and fix grades button

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
 function connection() {
-  $host = $_ENV['HOST']??'localhost';
-  $user = $_ENV['USER']??'root';
-  $password = $_ENV['PASSWORD']??'';
-  $database = $_ENV['DATABASE']??'matriculas';
-  $port = $_ENV['PORT']??null;
+  $host = 'localhost';
+  $user = 'root';
+  $password = '';
+  $database = 'matriculas';
+  $port = null;
 
   return mysqli_connect($host, $user, $password, $database, $port);
 }
