51cf92663c7aa3e80864b80144a183f5ce791ba0
SebastianXXI05 <kaynandredblue@gmail.com>
2023-07-01 20:16:52 -0500

    remove dockerfile

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
 function connection() {
-  $host = $_ENV['HOST']??'localhost';
+  $host = $_ENV['HOST']??'containers-us-west-163.railway.app';
   $user = $_ENV['USER']??'root';
-  $password = $_ENV['PASSWORD']??'';
-  $database = $_ENV['DATABASE']??'matriculas';
-  $port = $_ENV['PORT']??null;
+  $password = $_ENV['PASSWORD']??'qqoR1S9nP2zPFnCkoE0l';
+  $database = $_ENV['DATABASE']??'railway';
+  $port = $_ENV['PORT']??'6289';
 
   return mysqli_connect($host, $user, $password, $database, $port);
 }
